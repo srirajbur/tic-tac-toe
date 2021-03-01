23cc62ce31cab9efe7d6709267e371d8c1f46ad6
Sriraj Bura <sriraj.bura16@gmail.com>
2021-03-01 17:26:19 -0500

    implemented player class

---
+++ tic_tac_toe.rb
@@ -0,0 +1,14 @@
+#make a method that prints tictactoe board unitl someone wins or its a tie along with that
+#switch players
+#Make 2 players in initialize method
+
+
+#use gets.strip to get users input
+
+
+class Player
+  def initialize(name)
+    @name = name
+  end
+
+end
