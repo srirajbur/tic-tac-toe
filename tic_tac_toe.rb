a347429d651ac4fd983167d3aa0ff2ad77006e2e
Sriraj Bura <sriraj.bura16@gmail.com>
2021-03-01 21:23:33 -0500

    implemented Game class

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -4,11 +4,25 @@
 
 
 #use gets.strip to get users input
+class Game
+  def initialize
+    @board = Board.new
+    @playerX = Player.new("X")
+    @playerO = Player.new("O")
+    @current_player = @playerX
+  end
+
+  def play
+    loop do
+      @board.render      
+
+  end
+end
 
 
 class Player
-  def initialize(name)
-    @name = name
+  def initialize(team)
+    @team = team
   end
 
 end
